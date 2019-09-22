cd67b40c0c1b9f4d0680baa5c632c97b0b774092
ntngus0503 <@DESKTOP-E6QNJ7M>
2019-09-22 15:19:52 +0900

    전체적으로수정

--- festival/urls.py
+++ festival/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('studentprogram/', views.scprogram, name='scprogram')
+    path('', views.scprogram, name='scprogram')
 ]
