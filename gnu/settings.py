4c8af010322341859e07777731f009394a492504
ntngus0503 <@DESKTOP-E6QNJ7M>
2019-09-24 01:43:01 +0900

    settings.py에앱등록위치변경

--- gnu/settings.py
+++ gnu/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'festival.apps.FestivalConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'festival.apps.FestivalConfig',
+
 ]
 
 MIDDLEWARE = [
