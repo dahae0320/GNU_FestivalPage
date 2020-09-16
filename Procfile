061044fb36f4d1d721b165adce6eba237bdb49eb
eunchae619 <eunchae619@naver.com>
2020-09-17 04:16:24 +0900

    heroku 배포 셋팅

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn gnu.wsgi --log-file -
--- gnu/settings.py
+++ gnu/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@mc8ln(5fv1)7!hw=dqpxwuq^ey8-+5e1m$4cygx+n10!vzhs0'
+SECRET_KEY = os.environ.get(
+    'DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
