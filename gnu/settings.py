02f4411cbac9e14325616ad388dcbf59c8f73333
eunchae619 <eunchae619@naver.com>
2020-09-17 04:34:47 +0900

    heroku 배포 셋팅

--- gnu/settings.py
+++ gnu/settings.py
@@ -24,9 +24,9 @@ SECRET_KEY = os.environ.get(
     'DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
