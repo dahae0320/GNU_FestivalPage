9d10c9d28c1a8f36cf870b7fb33dc0861bd05254
eunchae619 <eunchae619@naver.com>
2020-09-17 06:45:31 +0900

    -

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\eunchae\desktop\programming\project\django\likelion2019\fgnu\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
